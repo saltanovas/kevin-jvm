26aa26005e4258271adc1880cf34c4a0bcf3e907
Grantas Gadliauskas <grantas0612@gmail.com>
2021-10-12 20:01:41 +0300

    MOBILE-236: Fix InitiatePaymentRefundResponse amount type (#9)
    Co-authored-by: Grantas Gadliauskas <grantas.gadliauskas@kevin.eu>

--- src/main/kotlin/eu/kevin/api/models/payment/initiatePaymentRefund/InitiatePaymentRefundResponse.kt
+++ src/main/kotlin/eu/kevin/api/models/payment/initiatePaymentRefund/InitiatePaymentRefundResponse.kt
@@ -1,18 +1,20 @@
-@file:UseSerializers(LocalDateTimeSerializer::class)
+@file:UseSerializers(LocalDateTimeSerializer::class, BigDecimalSerializer::class)
 
 package eu.kevin.api.models.payment.initiatePaymentRefund
 
 import eu.kevin.api.models.payment.StatusGroup
+import eu.kevin.api.serializers.BigDecimalSerializer
 import eu.kevin.api.serializers.LocalDateTimeSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
+import java.math.BigDecimal
 import java.time.LocalDateTime
 
 @Serializable
 data class InitiatePaymentRefundResponse(
     val id: Int,
     val paymentId: String,
-    val amount: String,
+    val amount: BigDecimal,
     val statusGroup: StatusGroup,
     val webhookUrl: String,
     val createdAt: LocalDateTime
