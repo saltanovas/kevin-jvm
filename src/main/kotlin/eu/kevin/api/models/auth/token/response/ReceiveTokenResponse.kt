3aa686d36b57ad55aa4a46f4c1b2c8910e149ac1
Luca Rospocher <luca.rospocher@kevin.eu>
2022-05-17 10:27:28 +0200

    Change authId from String to UUID

--- src/main/kotlin/eu/kevin/api/models/auth/token/response/ReceiveTokenResponse.kt
+++ src/main/kotlin/eu/kevin/api/models/auth/token/response/ReceiveTokenResponse.kt
@@ -1,10 +1,13 @@
 package eu.kevin.api.models.auth.token.response
 
+import eu.kevin.api.serializers.UUIDSerializer
 import kotlinx.serialization.Serializable
+import java.util.UUID
 
 @Serializable
 data class ReceiveTokenResponse(
-    val authId: String? = null,
+    @Serializable(with = UUIDSerializer::class)
+    val authId: UUID? = null,
     val tokenType: String,
     val accessToken: String,
     val accessTokenExpiresIn: Long,
