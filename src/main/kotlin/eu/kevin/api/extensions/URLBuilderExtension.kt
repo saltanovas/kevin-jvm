87ebb9ff0582f71dea312d3efcbf3830cea623dd
Šaltanovas Aivaras <aivaras.saltanovas@kevin.eu>
2023-06-28 01:24:55 +0300

    Mark extension method as internal

--- src/main/kotlin/eu/kevin/api/extensions/URLBuilderExtension.kt
+++ src/main/kotlin/eu/kevin/api/extensions/URLBuilderExtension.kt
@@ -2,6 +2,6 @@ package eu.kevin.api.extensions
 
 import io.ktor.http.URLBuilder
 
-fun URLBuilder.appendPath(path: String) {
+internal fun URLBuilder.appendPath(path: String) {
     pathSegments = path.split("/").filter { it.isNotBlank() }
 }
